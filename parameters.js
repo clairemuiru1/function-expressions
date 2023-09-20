1d75f7bf7fbc7c747cfc08a2b10e24f220e93498
clairemuiru1 <muiruclaire@gmail.com>
2023-09-20 15:23:25 +0300

    commit

---
+++ parameters.js
@@ -0,0 +1,16 @@
+// function favgame(game) {
+    // console.log(`My favourite game is ${game}`)
+// }
+// favgame("SONIC 2");
+// favgame("skyr in");
+// favgame("fallout 3");
+
+// function ninjaTurtle(name,weapon) {
+    // console.log(`${name} uses the ${weapon}`);
+// }
+// ninjaTurtle("Donatello","Bo staff");
+
+function add(num1,num2) {
+console.log(`${num1}+${num2}`);
+}
+add(5,2);
